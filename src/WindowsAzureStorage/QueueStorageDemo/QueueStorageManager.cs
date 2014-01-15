5f286fe7e93de9ba480065a669d40469a7dd0950
Agus Kurniawan <agusk2007@gmail.com>
2014-01-15 18:56:46 +0100

    updated queue storage

--- src/WindowsAzureStorage/QueueStorageDemo/QueueStorageManager.cs
+++ src/WindowsAzureStorage/QueueStorageDemo/QueueStorageManager.cs
@@ -5,10 +5,7 @@
 //////////////////////////////////////////////////////
 
 using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.WindowsAzure.Storage.Queue;
 using System.Configuration;
