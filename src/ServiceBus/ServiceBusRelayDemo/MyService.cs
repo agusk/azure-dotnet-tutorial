119a1b5aaa41f2035eb8a99f9b807144762a5c08
Agus Kurniawan <agusk2007@gmail.com>
2014-01-18 23:45:58 +0100

    updated service bus relay

---
+++ src/ServiceBus/ServiceBusRelayDemo/MyService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServiceBusRelayDemo
+{
+    class MyService : IMyService
+    {
+        public int Calculate(int a, int b, int c)
+        {
+            return a * b + c;
+        }
+    }
+}
