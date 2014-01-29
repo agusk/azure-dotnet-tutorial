3b601f0363edcce0fadb3252f7a280f7ea3c3e0c
Agus Kurniawan <agusk2007@gmail.com>
2014-01-29 16:44:24 +0100

    updated service bus topic

--- src/ServiceBus/ServiceBusTopicSubscriptionReceiver/Program.cs
+++ src/ServiceBus/ServiceBusTopicSubscriptionReceiver/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+using System.Configuration;
 
 namespace ServiceBusTopicSubscriptionReceiver
 {
@@ -10,6 +14,40 @@ namespace ServiceBusTopicSubscriptionReceiver
     {
         static void Main(string[] args)
         {
+            string cloudString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+            string topicName = "mytopic";
+            string subscriptionName = "mysubscription";
+            try
+            {
+                SubscriptionClient client = SubscriptionClient.CreateFromConnectionString(cloudString, topicName, subscriptionName);
+                while (true)
+                {
+                    BrokeredMessage message = client.Receive();
+
+                    if (message != null)
+                    {
+                        try
+                        {
+                            Console.WriteLine("Body: " + message.GetBody<string>());
+                            Console.WriteLine("MessageID: " + message.MessageId);
+                          
+
+                            // Remove message from subscription
+                            message.Complete();
+                        }
+                        catch (Exception)
+                        {
+                            // unlock message in subscription
+                            message.Abandon();
+                        }
+                    }
+                } 
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
         }
     }
 }
--- src/ServiceBus/ServiceBusTopicSubscriptionSender/Program.cs
+++ src/ServiceBus/ServiceBusTopicSubscriptionSender/Program.cs
@@ -3,6 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.ServiceBus;
+using System.Threading;
+using Microsoft.ServiceBus.Messaging;
+using System.Configuration;
 
 namespace ServiceBusTopicSubscriptionSender
 {
@@ -10,6 +14,26 @@ namespace ServiceBusTopicSubscriptionSender
     {
         static void Main(string[] args)
         {
+            string cloudString = ConfigurationManager.AppSettings["Microsoft.ServiceBus.ConnectionString"];
+            string topicName = "mytopic";
+            try
+            {
+                TopicClient client = TopicClient.CreateFromConnectionString(cloudString, topicName);
+
+                for(int i=0;i<10;i++)
+                {
+                    BrokeredMessage msg = new BrokeredMessage("message " + i.ToString());
+
+                    client.Send(msg);
+                    Console.WriteLine("message " + i.ToString() + "was sent");
+                    Thread.Sleep(5000);
+                }
+                
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
         }
     }
 }
