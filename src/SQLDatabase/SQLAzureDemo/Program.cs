b84a6dc2add6b69ac944ee259a7c406cc1b3b6e1
Agus Kurniawan <agusk2007@gmail.com>
2014-01-31 07:45:10 +0100

    updated to test connection for sql azure

--- src/SQLDatabase/SQLAzureDemo/Program.cs
+++ src/SQLDatabase/SQLAzureDemo/Program.cs
@@ -10,6 +10,8 @@ namespace SQLAzureDemo
     {
         static void Main(string[] args)
         {
+            SQLAzureManager sqlazure = new SQLAzureManager();
+            sqlazure.TestConnection();
         }
     }
 }
---
+++ src/SQLDatabase/SQLAzureDemo/SQLAzureManager.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace SQLAzureDemo
+{
+    class SQLAzureManager
+    {
+        private string sqlConnString;
+
+        public SQLAzureManager()
+        {
+            sqlConnString = ConfigurationManager.AppSettings["SQLAzure"];
+        }
+
+        public void TestConnection()
+        {
+            try
+            {
+                SqlConnection conn = new SqlConnection(sqlConnString);
+                conn.Open();
+                Console.WriteLine("Connected to SQL Azure");
+
+                conn.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+    }
+}
