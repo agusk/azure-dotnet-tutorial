29066f41be97f230a6b26f36d776c685f3359626
Agus Kurniawan <agusk2007@gmail.com>
2014-02-08 08:37:23 +0100

    updated media service

--- src/MediaService/AzureDotnet.MediaService/Class1.cs
+++ src/MediaService/AzureDotnet.MediaService/Class1.cs
@@ -4,13 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace UploadDemo
+namespace AzureDotnet.MediaService
 {
-    class Program
+    public class Class1
     {
-        static void Main(string[] args)
-        {
-
-        }
     }
 }
